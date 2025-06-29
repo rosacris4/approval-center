21f7beaf15d962690672632be71885cadfe1d42d
Rosa Chicafo <rosacristinachicafo@gmail.com>
2025-06-29 20:19:14 +0100

    algumas alteracoes

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}',
+    './app/**/*.{vue,js,ts}',
+    './composables/**/*.{js,ts}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
